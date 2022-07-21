c8f5b2b7a1520e5798160b0fcb4cb1c260886ecd
grochavieiranescara <guilherme.rocha@nescara.com>
2022-07-21 13:59:53 -0300

    update server.js

--- src/server.js
+++ src/server.js
@@ -29,6 +29,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+  res.status(200).send({
+    message: "Servidor funcionando...",
+  });
+});
+
 app.post("/api/email", (req, res) => {
   const { email, subject, message } = req.body;
   mailgun()
